daeb7c26a195dd20745ae773b6f285d357e1c6b5
igor-lourenco <igor.lourencosantos@gmail.com>
2025-10-03 09:57:02 -0300

    Inicio do projeto

---
+++ src/main/java/com/categoria/criacionais/_1_factory_method/_1_antes/Main.java
@@ -0,0 +1,9 @@
+package com.categoria.criacionais._1_factory_method._1_antes;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Oi.. :-)");
+    }
+}
